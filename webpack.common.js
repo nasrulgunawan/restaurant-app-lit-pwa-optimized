0c55151769496373ffb47546aa60e8cc2104fb5c
Nasrul Gunawan <nasrul.gunawan@nutrifood.co.id>
2021-07-05 22:54:39 +0700

    Add manifest, icons, and sw config

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -39,11 +40,8 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, 'src/assets/'),
+          from: path.resolve(__dirname, 'src/public/'),
           to: path.resolve(__dirname, 'dist/'),
-          globOptions: {
-            ignore: path.resolve(__dirname, 'src/assets/images/'),
-          },
         },
       ],
     }),
