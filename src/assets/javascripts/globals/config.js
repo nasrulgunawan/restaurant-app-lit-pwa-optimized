966c4f39653e9ef2522f8f671bf051ac6396c890
Nasrul Gunawan <nasrul.gunawan@nutrifood.co.id>
2021-07-06 12:37:40 +0700

    Add global config and websocket initiator

---
+++ src/assets/javascripts/globals/config.js
@@ -0,0 +1,13 @@
+const baseUrl = 'https://restaurant-api.dicoding.dev/';
+
+const CONFIG = {
+  BASE_URL: baseUrl,
+  BASE_IMAGE_URL: `${baseUrl}images/medium/`,
+  CACHE_NAME: 'Restaurant-App-V1',
+  DATABASE_NAME: 'restaurant-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'restaurants',
+  WEB_SOCKET_SERVER: 'wss://',
+};
+
+export default CONFIG;
